b79696e6c860af684b5f102fd61de34972407450
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2012-07-06 15:47:30 +0200

    Made some initial steps with the new Meteor app

--- meteor/meteor.js
+++ meteor/meteor.js
@@ -1,13 +1,27 @@
+Commits = new Meteor.Collection("commits");
+AllTimeCommitters = new Meteor.Collection("all_time_committers");
+
 if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to meteor.";
+  Template.latest_commits.commits = function () {
+    return Commits.find({}, {sort: {authored_date: -1}});
   };
 
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.all_time_committers.commits = function () {
+    return AllTimeCommitters.find({}, {sort: {commits: 1}});
+  };
+
+  Template.latest_commits.selected = function () {
+    return Session.equals("selected_commit", this._id) ? "selected" : "";
+  };
+
+  Template.latest_commits.events = {
+    'click div#commit': function () {
+      if (Session.equals("selected_commit", this._id)) {
+        Session.set('selected_commit', '');
+      } else {
+        Session.set('selected_commit', this._id);
+      }
+      
     }
   };
 }
